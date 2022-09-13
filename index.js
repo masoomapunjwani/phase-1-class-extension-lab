b418ba5534c016828e789ab206c8b8f6b953a980
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-09-13 17:17:22 +0300

    completed task

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+class Polygon {
+    constructor(Array) {
+        this.sides = Array;
+    }
+
+    get countSides () {
+        return this.sides.length;
+    }
+
+    get perimeter () {
+        return this.sides.reduce((sum, side) => sum + side, 0);
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+
+        const check1 = this.sides[0] + this.sides[1] > this.sides[2];
+        const check2 = this.sides[0] + this.sides[2] > this.sides[1];
+        const check3 = this.sides[1] + this.sides[2] > this.sides[0];
+        return ( check1 && check2 && check3 );
+    }
+}
+
+class Square extends Polygon {
+
+    get isValid() {
+        return (this.sides[0] === this.sides[1]) && (this.sides[2] === this.sides[3]);
+    }
+
+    get area() {
+        return this.sides[0] * this.sides[0];
+
+    }
+}
